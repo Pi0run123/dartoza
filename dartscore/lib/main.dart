03eaca017e34c7b91170bb16c34807a45b95804b
Pi0run123 <patryk.piorunski@edu.uekat.pl>
2026-05-05 21:12:36 +0200

    123

--- dartscore/lib/main.dart
+++ dartscore/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'pages/scoring_page.dart';
